2b030fed60d46e03279d796a85c3e1ea10c96f55
HaveAMango <dagolovinad@gmail.com>
2020-03-11 21:18:28 +0200

    adc

---
+++ src/main/java/lv/accenture/bootcamp/rardb/RardbApplication.java
@@ -0,0 +1,13 @@
+package lv.accenture.bootcamp.rardb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RardbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RardbApplication.class, args);
+	}
+
+}
