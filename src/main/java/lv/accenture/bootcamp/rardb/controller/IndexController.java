e4e1ca0565224901c7c101ba51422157ab3081ef
haveamango <dagolovinad@gmail.com>
2020-03-19 22:11:11 +0200

    login html in progress

--- src/main/java/lv/accenture/bootcamp/rardb/controller/IndexController.java
+++ src/main/java/lv/accenture/bootcamp/rardb/controller/IndexController.java
@@ -1,7 +1,10 @@
 package lv.accenture.bootcamp.rardb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import lv.accenture.bootcamp.rardb.login.User;
 
 @Controller
 public class IndexController {
@@ -10,9 +13,12 @@ public class IndexController {
 	public String toIndex() {
 		return "index";
 	}
+	
+	
 
 	@GetMapping("/login")
-	public String login() {
+	public String login(Model model) {
+		model.addAttribute("user", new User());
 		return "login";
 	}
 
